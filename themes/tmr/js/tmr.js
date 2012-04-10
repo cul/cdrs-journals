2a27e86212cdaea26becdefdb2e0dea111f3d57e
Jackson Harvell <jh3124@columbia.edu>
2012-04-10 13:30:56 -0400

    rm moved files

--- themes/tmr/js/tmr.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    // $() will work as an alias for jQuery() inside of this function
-   $('#meta-toggle').click(function(){$('#essay-meta').toggle('blind');});
-   
-   $("ul#author-index>li").tsort({attr:"id"});
-   
-  $("div#essay>p").numberParagraph();
-   
-});
--- themes/tmr/wpalchemy/metaboxes/setup.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once CFCT_PATH.'wpalchemy/MetaBox.php';
- 
-// global styles for the meta boxes
-if (is_admin()) wp_enqueue_style('wpalchemy-metabox', get_stylesheet_directory_uri() . '/wpalchemy/metaboxes/meta.css');
-
-/* eof */
