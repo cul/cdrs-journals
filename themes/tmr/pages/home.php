f1495a39c1edd318c4b55fc32a98cb4aaa1358b0
Jackson Harvell <jh3124@columbia.edu>
2012-04-10 10:48:12 -0400

    added home page templates

---
+++ themes/tmr/pages/home.php
@@ -0,0 +1,49 @@
+<?php
+
+// This file is part of the Carrington JAM Theme for WordPress
+// http://carringtontheme.com
+//
+// Copyright (c) 2008-2010 Crowd Favorite, Ltd. All rights reserved.
+// http://crowdfavorite.com
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+get_header();
+
+?>
+
+<div class="span-12 prepend-6 content">
+
+<?
+
+
+query_posts( 'post_type=hp-feature');
+
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+cfct_template_file('content', 'hp-feature');	}
+}
+
+
+?>
+
+</div>
+
+<?
+/* comments_template(); */
+
+/* get_sidebar(); */
+get_footer();
+
+?>
