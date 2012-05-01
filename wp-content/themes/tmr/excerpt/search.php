0f06001e9ffd48cc33512abd1a652f9c3fa4895b
Jackson Harvell <jh3124@columbia.edu>
2012-05-01 18:10:32 -0400

    more css refinement

--- wp-content/themes/tmr/excerpt/search.php
+++ wp-content/themes/tmr/excerpt/search.php
@@ -14,17 +14,32 @@
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 // **********************************************************************
-
+ 
+global $post;
+ 
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
-if (have_posts()) {
-	while (have_posts()) {
-		the_post();
-		cfct_template_file('excerpt', 'search');
-	}
-} else {
-	cfct_misc('no-results');
-}
-
 ?>
+ 
+
+ 
+<a class='title-link' href="<?php the_permalink() ?>"><?php the_title(); ?></a> 
+<?php 
+
+
+
+the_excerpt();
+
+ 
+ 
+/*
+the_time('F j, Y');
+
+the_category(', ');
+
+comments_popup_link(__('No comments', 'carrington-jam'), __('1 comment', 'carrington-jam'), __('% comments', 'carrington-jam'));
+
+*/
+?>
+ <!-- .excerpt -->
