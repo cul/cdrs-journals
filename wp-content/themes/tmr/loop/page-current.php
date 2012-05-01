14e7c5983b186edce449b78ff9ebcbbb7e2c7abd
Jackson Harvell <jh3124@columbia.edu>
2012-05-01 15:59:48 -0400

    format name function in tax lists

--- wp-content/themes/tmr/loop/page-current.php
+++ wp-content/themes/tmr/loop/page-current.php
@@ -39,7 +39,7 @@ $authors = get_the_terms( $post->ID, 'author' );
 if($authors){
 
 foreach($authors as $term){
-      echo '<a class="author" href="'.get_term_link($term->slug, 'author').'">'.$term->name.'</a>'; 
+      echo '<a class="author" href="'.get_term_link($term->slug, 'author').'">'.formatName($term->name).'</a>'; 
 
 }
 
