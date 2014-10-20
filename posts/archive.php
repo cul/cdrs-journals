9ee5b946529a8bba48698832e98724fce8f2e030
CDRS Unix Admin <info@cdrs.columbia.edu>
2014-10-20 10:54:43 -0400

    fixed section display

--- posts/archive.php
+++ posts/archive.php
@@ -33,7 +33,7 @@ get_header();
 			printf(__('Issue: %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
 		} elseif (is_tax('authors')) {
 			printf(__('Author Archives: %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
-		}elseif (is_tax('section')) {
+		}elseif (is_tax('sections')) {
 			printf(__(' %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
 		}elseif (is_category()) {
 			printf(__('%s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
