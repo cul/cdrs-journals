94fdeff54a75767f5271597bb789a6eb10c75ee7
Megan O'Neill <meganoneill@LTMLOAN3.local>
2014-10-22 12:49:51 -0400

    conditional doi/citation display

--- content/type-article.php
+++ content/type-article.php
@@ -109,12 +109,17 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 		$custom_doi = get_post_custom($post->ID);
   		$the_doi = $custom_doi['doi'];
+  		if($the_doi[0]){
   		echo '<h4>DOI: <a href="dx.doi.org/'. $the_doi[0] . '">' . $the_doi[0] . '</a></h4>';
+  		}
+
 
 		$citation_info = get_post_custom($post->ID);
   		$the_citation = $citation_info['citation'];
+  		if($the_citation[0]){
   		echo '<h4>Citation: ' . $the_citation[0] . '</h4>';
-  		echo '</br>';
+  		echo '</br>'
+  		};
 
   		if ( has_post_thumbnail() ) {
 				echo the_post_thumbnail();
