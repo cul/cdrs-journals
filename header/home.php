36ac01abc91c673455257a63687a9d14b21bd40e
Megan O'Neill <mo2638@columbia.edu>
2014-10-30 15:49:41 -0400

    site description location change

--- header/home.php
+++ header/home.php
@@ -94,10 +94,10 @@ $title_description = (is_home() && !empty($blog_desc) ? ' - '.$blog_desc : '');
 
 <p class="tagline col-sm-10 col-sm-offset-1">
 
- <?php $blog_info =  get_option( 'general-options' );
-  $words = $blog_info['site_desc']; 
-  $trim_words = wp_trim_words( $words, $num_words = 55, $more = null ); 
-  echo $trim_words; ?>
+ <?php $blog_info =  get_option( 'site_desc' );
+    $str = str_replace('\\', '', $blog_info);
+    echo $str;
+   ?>
  
 </p>
 
