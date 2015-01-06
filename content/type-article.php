4115513f895f2d4f534d3cf72dd1bd58be3d7980
Megan O'Neill <mo2638@columbia.edu>
2015-01-06 10:35:40 -0500

    thumbnail img on article for mobile

--- content/type-article.php
+++ content/type-article.php
@@ -159,16 +159,25 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 	?>
 	</div>
-	<div class="featured_image col-sm-12">
+	<?php
+		if ( has_post_thumbnail() ) { ?>
+	<div class="featured_image col-md-12 hidden-sm hidden-xs">
 		<?php
-		if ( has_post_thumbnail() ) {
 			    the_post_thumbnail('full');
 				echo '<h3 class="featured_image_caption">' . get_post( get_post_thumbnail_id() )->post_excerpt . '</h3>';
 				$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-		}
 		?>
 	</div>
-
+	<div class="featured_image col-xs-21 hidden-md hidden-lg">
+		<?php
+			    the_post_thumbnail();
+				echo '<h3 class="featured_image_caption">' . get_post( get_post_thumbnail_id() )->post_excerpt . '</h3>';
+				$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+		?>
+	</div>
+	
+	<?php } ?>
+	
 	</header>
 	<div class="entry-content">
 		<?php the_content() ?>
@@ -229,8 +238,9 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 <a href="http:///www.linkedin.com/shareArticle?mini=true&url=<?php echo $link ?>&title=<?php echo the_title() ?>" target="_blank"> <i class="fa fa-linkedin-square"></i>&nbsp;LinkedIn</a>
 
 </div>
+
 <div class="mobile_social_media visible-xs">
-<a href="mailto:?subject=<?php echo the_title(); ?>&body=<?php echo the_permalink(); ?>"><i class="fa fa-envelope fa-1x"></i></a>
+<a href="mailto:?subject=<?php echo the_title(); ?>&body=<?php echo the_permalink(); ?>"><span class="fa-stack fa-lg"><i class="fa fa-square-o fa-stack-2x"></i> <i class="fa fa-envelope fa-1x"></i></span></a>
 
 <!-- example for email link: <a href="mailto:onecooldude@gmail.com?subject=Hey+Dude.+You're+Cool.&cc=anotherdude@gmail.com&bcc=invisibledude@gmail.com&body=Your+awesome+message+goes+here.%0D%0A%0D%0AThis+is+on+a+new+line.+Go+to+http%3A%2F%2Fwww.google.com%2F.">onecooldude@gmail.com</a> -->
 
@@ -238,12 +248,12 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 	    if($twitter_name){
 	?>
-<a href="http://twitter.com/intent/tweet?text=<?php echo the_title() ?>&url=<?php echo $link ?>&via=<?php echo $twitter_name ?>" target="_blank"> <i class="fa fa-twitter fa-1x"></i></a>
+<a href="http://twitter.com/intent/tweet?text=<?php echo the_title() ?>&url=<?php echo $link ?>&via=<?php echo $twitter_name ?>" target="_blank"><span class="fa-stack fa-lg"><i class="fa fa-square-o fa-stack-2x"></i>  <i class="fa fa-twitter fa-1x"></i></span></a>
 	<?php }
 		if($fb_name){
 	?>
-<a href="http://facebook.com//sharer/sharer.php?u=<?php echo $link ?>" target="_blank"> <i class="fa fa-facebook fa-1x"></i></a>
+<a href="http://facebook.com//sharer/sharer.php?u=<?php echo $link ?>" target="_blank"> <span class="fa-stack fa-lg"><i class="fa fa-square-o fa-stack-2x"></i> <i class="fa fa-facebook fa-1x"></i></span></a>
 	<?php } ?>
-<a href="http:///www.linkedin.com/shareArticle?mini=true&url=<?php echo $link ?>&title=<?php echo the_title() ?>" target="_blank"> <i class="fa fa-linkedin-square fa-1x"></i></a>
+<a href="http:///www.linkedin.com/shareArticle?mini=true&url=<?php echo $link ?>&title=<?php echo the_title() ?>" target="_blank"><span class="fa-stack fa-lg"><i class="fa fa-square-o fa-stack-2x"></i> <i class="fa fa-linkedin fa-1x"></i></span></a>
 </div>
 </div>
