773349a882398db52aacc055c578b1a75191abf1
CDRS Unix Admin <info@cdrs.columbia.edu>
2015-11-11 13:48:40 -0500

    add back navbar functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,13 @@ jQuery( document ).ready(function() {
   jQuery(".mobile_social_media").insertAfter(".library_data");
 
 
-  // jQuery("body.home #masthead").waypoint(function() {
-  //   jQuery("nav").toggle();
-  //   }, {
-  //   offset: function() {
-  //     return -jQuery(this).height();
-  //   }
-  // });
+   jQuery("body.home #masthead").waypoint(function() {
+     jQuery("nav").toggle();
+     }, {
+     offset: function() {
+       return -jQuery(this).height();
+     }
+   });
 
   jQuery("article header").waypoint(function() {
     jQuery("#scrolled-head").toggle();
