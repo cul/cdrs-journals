c35d5a2e08926d573a9e095ce59f779dcd27e51c
CDRS Unix Admin <info@cdrs.columbia.edu>
2015-12-10 14:20:32 -0500

    register_sidebar call update for upgrade

--- wp-content/themes/tmr/functions/sidebars.php
+++ wp-content/themes/tmr/functions/sidebars.php
@@ -21,6 +21,7 @@ if (function_exists('register_sidebar')) {
 	register_sidebar(
 		array(
 			'name' => 'Primary Sidebar',
+			'id' => 'primary-sidebar',
 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
 			'after_widget' => '<div class="clear"></div></div>',
 			'before_title' => '<h2 class="widget-title">',
@@ -30,6 +31,7 @@ if (function_exists('register_sidebar')) {
 	register_sidebar(
 		array(
 			'name' => 'Secondary Sidebar',
+			'id' => 'secondary-sidebar',
 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
 			'after_widget' => '<div class="clear"></div></div>',
 			'before_title' => '<h2 class="widget-title">',
