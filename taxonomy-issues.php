75855f21a5a2703136de077c853929433eb1d890
Ed Madrid <edmadrid@gmail.com>
2016-09-12 12:04:53 -0400

    fixes issue with disappearing bibliography field

--- taxonomy-issues.php
+++ taxonomy-issues.php
@@ -28,64 +28,56 @@ if($options['featured_image_setting'] == "yes"){
 
 <div id="primary" class="col-sm-12 <?php echo $featured_class ?>">
 
-	<header role="page-title">
-		<h1 class="archive-title"><?php
-			if (is_day()) {
-				printf(__('Daily Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date() . '</span>');
-			} elseif (is_month()) {
-				printf(__('Monthly Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date(_x('F Y', 'monthly archives date format', 'carrington-blueprint')) . '</span>');
-			} elseif (is_year()) {
-				printf(__('Yearly Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date(_x('Y', 'yearly archives date format', 'carrington-blueprint')) . '</span>');
-			}  elseif (is_tax('issues')) {
-				printf(__('Issue: %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
-			}elseif (is_tax('aauthor')) {
-				printf(__('Author Archives: %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
-			}
-		?></h1>
+<header role="page-title">
+	<h1 class="archive-title"><?php
+		if (is_day()) {
+			printf(__('Daily Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date() . '</span>');
+		} elseif (is_month()) {
+			printf(__('Monthly Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date(_x('F Y', 'monthly archives date format', 'carrington-blueprint')) . '</span>');
+		} elseif (is_year()) {
+			printf(__('Yearly Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date(_x('Y', 'yearly archives date format', 'carrington-blueprint')) . '</span>');
+		}  elseif (is_tax('issues')) {
+			printf(__('Issue: %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
+		}elseif (is_tax('aauthor')) {
+			printf(__('Author Archives: %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
+		}
+	?></h1>
 
-		<!-- issue taxonomy print date -->
+	<!-- issue taxonomy print date -->
 
-		<div class="issue-print-date">
-			<?php
-				//Get the correct taxonomy ID by slug
-				$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+	<?php
+	// get the correct taxonomy ID by slug
+	$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+	// get taxonomy meta
+	$print_date = get_tax_meta($term->term_id,'print_date');
+	if($print_date != '') : ?>
+		<?php echo '<div class="issue-print-date">' ?>
+		<?php echo $print_date; ?>
+		<?php echo '</div>' ?>
+	<?php endif; ?>
 
-				//Get Taxonomy Meta
-				$saved_data = get_tax_meta($term->term_id,'print_date');
-				echo $saved_data;
-			?>
-		</div>
-
-	</header>
+</header>
 
 <!-- issue taxonomy description field  -->
 
 <?php
 	$termDescription = term_description( '', get_query_var( 'taxonomy' ) );
 	if($termDescription != '') : ?>
-		<h3 class="subhead"><?php echo $termDescription; ?></h3>
-<?php endif; ?>
+		<?php echo '<h3 class="subhead">' ?>
+		<?php echo $termDescription; ?>
+		<?php echo '</h3>' ?>
+	<?php endif; ?>
 
 <!-- issue taxonomy bibliography field  -->
 
 <?php
-	$issue_link;
-	$post_terms = (wp_get_post_terms($post->ID, 'issues'));
-	$the_issue = wp_list_filter($post_terms, array('slug'=>'current-issue'),'NOT');
-	foreach ($the_issue as $issue) {
-		echo $issue_link;
-	}
-?>
-
-<?php if ( get_tax_meta($issue->term_id, 'bibliography', true) ) { ?>
-
-<?php
-	$saved_data = get_tax_meta($issue->term_id,'bibliography');
-	echo '<h3 class="section-label"><span>Bibliography</span></h3><div class="bibliography">';
-	echo $saved_data;
-	echo '</div>';
-?>
-<?php } ?>
+	$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+	$bib_data = get_tax_meta($term->term_id,'bibliography');
+	if($bib_data != '') : ?>
+		<?php echo '<h3 class="section-label"><span>Bibliography</span></h3><div class="bibliography">' ?>
+		<?php echo $bib_data; ?>
+		<?php echo '</div>' ?>
+	<?php endif; ?>
 
 <!-- issue taxonomy articles  -->
 
