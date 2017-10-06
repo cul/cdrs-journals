e8d86d328250f49421d8314888380f332eed9cb2
Ed Madrid <edmadrid@gmail.com>
2017-10-06 13:55:19 -0400

    modifies deploy configs

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,4 +1,4 @@
 server "#{fetch(:department)}-nginx-#{fetch(:stage)}1.cul.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
 ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
 
-set :url, 'https://journals.cdrs.columbia.edu'
+set :url, 'https://journals-test.cul.columbia.edu'
