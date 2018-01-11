59c89f62bbd04a8da2a1bab01726c89a691406f7
Ed Madrid <edmadrid@gmail.com>
2018-01-11 13:46:31 -0500

    modifies category excerpt bylines

--- wp-content/themes/cdrs_journals/excerpt/excerpt-category.php
+++ wp-content/themes/cdrs_journals/excerpt/excerpt-category.php
@@ -42,12 +42,12 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 <h2 class="entry-title"><a href="<?php the_permalink() ?>"  title="<?php printf( esc_attr__( 'Permalink to %s', 'carrington-blueprint' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h2>
 
 <p class="excerpt-byline">
-	Published <?php the_time('M n, Y'); ?>
+
 	<?php
 	  		$authors =  wp_get_object_terms($post->ID, 'authors', array("fields" => "all", 'orderby' => 'term_order'));
 	  		$more_authors = array();
 	  		if($authors){
-					echo 'by ';
+					echo '';
 	           foreach ( $authors as $author ) {
 
 	    		// The $term is an object, so we don't need to specify the $taxonomy.
@@ -64,6 +64,8 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 			echo implode(', ', $more_authors);
 	   }?>
 </p>
+
+<p class="pubdate">Published <?php the_time('M n, Y'); ?></p>
 
 <p class="excerpt-text"><?php the_excerpt(); ?></p>
 
