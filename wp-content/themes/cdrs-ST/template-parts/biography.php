1e2ec54eb1835a52c6dcc4c5e14ebfb34a053616
Ed Madrid <edmadrid@gmail.com>
2018-05-07 10:37:47 -0400

    multiple author bio boxes for posts using coauthors plugin

--- wp-content/themes/cdrs-ST/template-parts/biography.php
+++ wp-content/themes/cdrs-ST/template-parts/biography.php
@@ -8,31 +8,26 @@
  */
 ?>
 
-<div class="author-info">
-	<div class="author-avatar">
-		<?php
-		/**
-		 * Filter the Twenty Sixteen author bio avatar size.
-		 *
-		 * @since Twenty Sixteen 1.0
-		 *
-		 * @param int $size The avatar height and width size in pixels.
-		 */
-		$author_bio_avatar_size = apply_filters( 'twentysixteen_author_bio_avatar_size', 42 );
+<?php
 
-		echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
-		?>
-	</div><!-- .author-avatar -->
+	if ( function_exists( 'coauthors' ) ) {
 
-	<div class="author-description">
-		<h2 class="author-title"><?php echo get_the_author(); ?></h2>
+	$coauthors = get_coauthors();
+	foreach( $coauthors as $coauthor ):
 
-		<p class="author-bio">
-			<?php if ( '' !== get_the_author_meta( 'description' ) ) {
-			the_author_meta( 'description' );
-			} ?>
-			<!-- <a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"> 	<?php printf( __( 'More by %s', 'twentysixteen' ), 'this author' ); ?> 
-			</a> -->
-		</p><!-- .author-bio -->
-	</div><!-- .author-description -->
-</div><!-- .author-info -->
+	echo '<div class="author-info"><div class="author-avatar">';
+	echo get_avatar( $coauthor->user_email, '42' );
+	echo '</div><div class="author-description"><h2 class="author-title">';
+
+	$userdata = get_userdata( $coauthor->ID );
+	if ( $userdata->display_name ) echo $userdata->display_name;
+
+	echo '</h2><p class="author-bio">';
+	if ( $userdata->user_description ) echo $userdata->user_description;
+
+	echo '</p></div></div><br>';
+
+	endforeach;
+	} else {
+	}
+?>
